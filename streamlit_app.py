cb1cb5a1cb19919e19e0ab7c6b1167ae415f4ac4
charitesh29 <76771694+charitesh29@users.noreply.github.com>
2024-01-19 16:23:51 +0530

    Update streamlit_app.py
    Fixed closing colon issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import streamlit
 streamlit.title('Welcome to the snowflake streamlit')
 
 streamlit.Header("Snowflake Learning's")
-streamlit.text('Architecture)
-streamlit.text('Basics)
+streamlit.text('Architecture')
+streamlit.text('Basics')
