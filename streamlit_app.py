36987cf64ccab91e08263a8c3b750570eee4f2c1
charitesh29 <76771694+charitesh29@users.noreply.github.com>
2024-01-19 16:23:09 +0530

    Update streamlit_app.py
    Fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('Welcome to the snowflake streamlit')
 
-streamlit.Header('Snowflake Learning's')
+streamlit.Header("Snowflake Learning's")
 streamlit.text('Architecture)
 streamlit.text('Basics)
